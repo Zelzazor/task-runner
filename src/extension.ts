1ed8617b367bf5e502b81251418a9e30c6cfc852
Felipe Ramírez <zelzazor.ramirez62@gmail.com>
2026-09-12 21:30:10 -0400

    Add Task Runner activity bar view scaffold
    Contributes a dedicated activity bar container and tree view with an
    empty-state welcome screen, replacing the generated Hello World command.

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,21 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+class EmptyTaskTreeDataProvider implements vscode.TreeDataProvider<never> {
+	getTreeItem(element: never): vscode.TreeItem {
+		return element;
+	}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "task-runner" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('task-runner.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from task-runner!');
-	});
-
-	context.subscriptions.push(disposable);
+	getChildren(): never[] {
+		return [];
+	}
 }
 
-// This method is called when your extension is deactivated
+export function activate(context: vscode.ExtensionContext) {
+	const treeView = vscode.window.createTreeView('taskRunner.tasksView', {
+		treeDataProvider: new EmptyTaskTreeDataProvider(),
+	});
+
+	context.subscriptions.push(treeView);
+}
+
 export function deactivate() {}
